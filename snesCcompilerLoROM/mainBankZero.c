45ddcd08c9dc1e14c14d456111c219f01bc541f0
Phillip <phillipmay@videotron.ca>
2018-11-15 19:51:41 -0500

    Added input example

--- snesCcompilerLoROM/mainBankZero.c
+++ snesCcompilerLoROM/mainBankZero.c
@@ -9,26 +9,66 @@ typedef	unsigned char byte;
 #include <STRING.H>
 #include <MALLOC.H>
 
+#include "include\mainBankZero.h"
 #include "include\SNES.h"
 #include "include\initsnes.h"
 
 char testString[40] = "Initial string in heap";
 void main(void){
 	//Variables
+	int8_t regRead1; //Variable for storing hardware registers
+	int8_t regRead2; //Variable for storing hardware registers
 	//Initialization
 	//Initialize the stack
 	initSNES(SLOWROM);
+	ButtonTest();
 
-	//Simple code to verify the program can do a thing
-	REG_CGADD = 0x00;
-	REG_CGDATA = 0x00;
-	REG_CGDATA = 0x78;	
-	
-	REG_INIDISP = 0x0F;
+	//Simple recreation of input test
 	while(1){
+
 	}
 }
 
 void far IRQHandler(void){
 	
+}
+
+//A little program that demonstrates reading inputs and waiting for VBlank
+void ButtonTest(void){
+	//Variables
+	int8_t regRead1; //Variable for storing hardware registers
+	int8_t regRead2; //Variable for storing hardware registers	
+	
+	REG_NMITIMEN = 0x01;
+	REG_TM  = 0x00;	//Set main screen
+	REG_INIDISP = 0x0F; //Turn screen on
+	
+	//Simple code to verify the program can do a thing
+	REG_CGADD = 0x00;
+	REG_CGDATA = 0x00;
+	REG_CGDATA = 0x78;	
+	
+	//Simple recreation of input test
+	while(1){
+		do{ //Wait for Vblank
+			regRead1 = REG_RDNMI;
+		} while( (regRead1 > 0));
+		do{ //Wait for H/V blank flag and joypad busy flag
+			regRead1 = REG_RDNMI;
+		} while( (regRead1 > 0));
+		
+		regRead1 = REG_JOY1L;
+		regRead2 = REG_JOY1H;
+		if ( (REG_JOY1L != 0) || (REG_JOY1H != 0)){
+			REG_CGADD = 0x00;
+			REG_CGDATA = 0x1F;
+			REG_CGDATA = 0x00;				
+		}
+		else{
+			REG_CGADD = 0x00;
+			REG_CGDATA = 0x00;
+			REG_CGDATA = 0x78;				
+		}
+		
+	}	
 }
