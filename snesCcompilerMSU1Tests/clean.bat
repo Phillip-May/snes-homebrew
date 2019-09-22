c315fdbe530304727047151697697c7b06426482
Phillip <phillipmay@videotron.ca>
2019-09-22 19:49:24 -0400

    Add files via upload

---
+++ snesCcompilerMSU1Tests/clean.bat
@@ -0,0 +1,8 @@
+del mainBankZero.obj -f
+del ressource.obj
+del vectors.obj -f
+del ROM.bin -f
+del lib\initsnes.obj -f
+del lib\kernel.obj -f
+del ROM.bnk -f
+del ROM.map -f
---
+++ snesCcompilerMSU1Tests/include/mainBankZero.h
@@ -0,0 +1,4 @@
+int termM0Init(void);
+int termM0PrintStringXY(char *szInput, int inpX, int inpY);
+void ButtonTest(void);
+void Mode0Text(void);
