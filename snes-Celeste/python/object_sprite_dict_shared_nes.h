43f934191f8af2af36181e36a5ae897b810a49e1
Phillip May <phillipmay@videotron.ca>
2026-01-04 22:57:54 +0800

    Shared header files

---
+++ snes-Celeste/python/object_sprite_dict_shared_nes.h
@@ -0,0 +1,73 @@
+// Shared object sprite dictionary for all levels (NES format - compact)
+// Generated from baseCelesteTileMap.json
+// Compact format: only used entries stored, with lookup table for fast access
+// Compact array format: [pal_idx, tl, tr, bl, br] (tile_idx not stored, use lookup table)
+
+#ifndef OBJECT_SPRITE_DICT_SHARED_NES_H
+#define OBJECT_SPRITE_DICT_SHARED_NES_H
+
+// Lookup table: maps tile_idx -> compact_index (0xFF = not found)
+const unsigned char object_sprite_lookup_table[33] = {
+    /* tile 0 */  255,
+    /* tile 1 */  0,
+    /* tile 2 */  1,
+    /* tile 3 */  2,
+    /* tile 4 */  3,
+    /* tile 5 */  4,
+    /* tile 6 */  5,
+    /* tile 7 */  6,
+    /* tile 8 */  7,
+    /* tile 9 */  255,
+    /* tile 10 */  255,
+    /* tile 11 */  8,
+    /* tile 12 */  9,
+    /* tile 13 */  255,
+    /* tile 14 */  255,
+    /* tile 15 */  255,
+    /* tile 16 */  255,
+    /* tile 17 */  255,
+    /* tile 18 */  10,
+    /* tile 19 */  11,
+    /* tile 20 */  12,
+    /* tile 21 */  255,
+    /* tile 22 */  13,
+    /* tile 23 */  14,
+    /* tile 24 */  15,
+    /* tile 25 */  16,
+    /* tile 26 */  17,
+    /* tile 27 */  255,
+    /* tile 28 */  18,
+    /* tile 29 */  255,
+    /* tile 30 */  255,
+    /* tile 31 */  255,
+    /* tile 32 */  255
+};
+
+// Compact sprite data array (only used entries)
+// Format: [pal_idx, tl, tr, bl, br]
+const unsigned char object_sprite_dict_compact[19][5] = {
+    /* compact_idx 0: tile 1 */  { 3, 1, 2, 3, 4 },
+    /* compact_idx 1: tile 2 */  { 3, 1, 2, 3, 5 },
+    /* compact_idx 2: tile 3 */  { 3, 6, 7, 8, 9 },
+    /* compact_idx 3: tile 4 */  { 3, 1, 2, 10, 4 },
+    /* compact_idx 4: tile 5 */  { 3, 11, 12, 13, 14 },
+    /* compact_idx 5: tile 6 */  { 3, 15, 16, 17, 18 },
+    /* compact_idx 6: tile 7 */  { 3, 19, 20, 21, 22 },
+    /* compact_idx 7: tile 8 */  { 0, 23, 24, 25, 26 },
+    /* compact_idx 8: tile 11 */  { 0, 30, 31, 0, 0 },
+    /* compact_idx 9: tile 12 */  { 0, 32, 33, 0, 0 },
+    /* compact_idx 10: tile 18 */  { 0, 46, 47, 48, 49 },
+    /* compact_idx 11: tile 19 */  { 0, 0, 0, 46, 47 },
+    /* compact_idx 12: tile 20 */  { 0, 50, 51, 52, 53 },
+    /* compact_idx 13: tile 22 */  { 0, 56, 57, 58, 59 },
+    /* compact_idx 14: tile 23 */  { 1, 60, 61, 62, 63 },
+    /* compact_idx 15: tile 24 */  { 1, 64, 65, 66, 67 },
+    /* compact_idx 16: tile 25 */  { 1, 68, 69, 70, 71 },
+    /* compact_idx 17: tile 26 */  { 0, 72, 55, 73, 74 },
+    /* compact_idx 18: tile 28 */  { 0, 77, 78, 79, 80 }
+};
+
+#define OBJECT_SPRITE_DICT_LOOKUP_TABLE_SIZE 33
+#define OBJECT_SPRITE_DICT_COMPACT_COUNT 19
+
+#endif // OBJECT_SPRITE_DICT_SHARED_NES_H
---
+++ snes-Celeste/python/object_sprite_dict_shared_snes.h
@@ -0,0 +1,68 @@
+// Shared object sprite dictionary for all levels (SNES format)
+// Generated from baseCelesteTileMap.json
+// SNES/Other: Format with tile_idx field for lookup
+// Each entry: [tile_idx, pal_idx, tl, tr, bl, br]
+
+#ifndef OBJECT_SPRITE_DICT_SHARED_SNES_H
+#define OBJECT_SPRITE_DICT_SHARED_SNES_H
+
+const unsigned char object_sprite_dict_shared[27][6] = {
+    // Entry 0
+    { 1, 3, 1, 2, 3, 4 },
+    // Entry 1
+    { 2, 3, 1, 2, 3, 5 },
+    // Entry 2
+    { 3, 3, 6, 7, 8, 9 },
+    // Entry 3
+    { 4, 3, 1, 2, 10, 4 },
+    // Entry 4
+    { 5, 3, 11, 12, 13, 14 },
+    // Entry 5
+    { 6, 3, 15, 16, 17, 18 },
+    // Entry 6
+    { 7, 3, 19, 20, 21, 22 },
+    // Entry 7
+    { 8, 0, 23, 24, 25, 26 },
+    // Entry 8
+    { 11, 0, 30, 31, 0, 0 },
+    // Entry 9
+    { 12, 0, 32, 33, 0, 0 },
+    // Entry 10
+    { 18, 0, 46, 47, 48, 49 },
+    // Entry 11
+    { 19, 0, 0, 0, 46, 47 },
+    // Entry 12
+    { 20, 0, 50, 51, 52, 53 },
+    // Entry 13
+    { 22, 0, 56, 57, 58, 59 },
+    // Entry 14
+    { 23, 1, 60, 61, 62, 63 },
+    // Entry 15
+    { 24, 1, 64, 65, 66, 67 },
+    // Entry 16
+    { 25, 1, 68, 69, 70, 71 },
+    // Entry 17
+    { 26, 0, 72, 55, 73, 74 },
+    // Entry 18
+    { 28, 0, 77, 78, 79, 80 },
+    // Entry 19
+    { 64, 2, 167, 168, 169, 112 },
+    // Entry 20
+    { 70, 2, 0, 0, 185, 186 },
+    // Entry 21
+    { 71, 2, 0, 0, 187, 188 },
+    // Entry 22
+    { 86, 2, 209, 210, 211, 212 },
+    // Entry 23
+    { 87, 2, 213, 214, 215, 216 },
+    // Entry 24
+    { 96, 0, 230, 231, 103, 103 },
+    // Entry 25
+    { 97, 0, 231, 232, 103, 103 },
+    // Entry 26
+    { 118, 0, 269, 247, 270, 0 }
+};
+
+#define OBJECT_SPRITE_DICT_SHARED_COUNT 27
+
+#endif // OBJECT_SPRITE_DICT_SHARED_SNES_H
